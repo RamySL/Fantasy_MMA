c3a17c5199648afa1c212626069a252e5bcac9c7
Ramy SAIL <ramyyahiasail@gmail.com>
2026-04-28 03:12:48 +0200

    feat: squelette basique pour le serveur.

---
+++ backend/server.go
@@ -0,0 +1,32 @@
+package main
+
+/*
+ce Squellette est pris de : https://gobyexample.com/http-servers 
+*/
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+    fmt.Fprintf(w, "hello weeee\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+    for name, headers := range req.Header {
+        for _, h := range headers {
+            fmt.Fprintf(w, "%v: %v\n", name, h)
+        }
+    }
+}
+
+func main() {
+
+    http.HandleFunc("/hello", hello)
+    http.HandleFunc("/headers", headers)
+
+    http.ListenAndServe(":8090", nil)
+}
